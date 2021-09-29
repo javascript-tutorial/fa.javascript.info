8314b6518d459f5163e47bb8b1b8df7c400728b6
Erfan Yeganegi <yeganegierfan@gmail.com>
2021-09-29 05:22:55 +0330

    translation complete

--- 2-ui/2-events/02-bubbling-and-capturing/bubble-target.view/script.js
+++ 2-ui/2-events/02-bubbling-and-capturing/bubble-target.view/script.js
@@ -2,7 +2,7 @@
 form.onclick = function(event) {
   event.target.style.backgroundColor = 'yellow';
 
-  // chrome needs some time to paint yellow
+  // مرورگر کروم مقداری زمان نیاز دارد تا زرد را نقاشی کند
   setTimeout(() => {
     alert("target = " + event.target.tagName + ", this=" + this.tagName);
     event.target.style.backgroundColor = ''
